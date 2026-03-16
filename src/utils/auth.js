c9adac7846650de7351aab3b43bdf1b6445b0ff8
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 03:48:53 +0530

    feat: integrate src/utils/auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+  try {
+    const verified = jwt.verify(token, 'secretkey');
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid token' });
+  }
+}
+
+module.exports = authenticateToken;
